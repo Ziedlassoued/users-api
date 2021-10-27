12035cccaa4642c4c4f9f3f40730df5b94f5ffb2
ziedlassoued <zied.lassoued@outlook.de>
2021-10-27 16:42:43 +0200

    Prepare login and me route

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,41 @@ const port = 3000;
 
 app.use(express.json());
 
-const users = ['David', 'Anke', 'Alice', 'Zied'];
+const users = [
+  {
+    name: 'David',
+    username: 'Dave',
+    password: 'aaa',
+  },
+  {
+    name: 'Anke',
+    username: 'Anke99',
+    password: 'bbb',
+  },
+  {
+    name: 'Alice',
+    username: 'Alice77',
+    password: 'kkk',
+  },
+  {
+    name: 'Zied',
+    username: 'Zied66',
+    password: 'bbb',
+  },
+];
+
+app.post('/api/login/', (request, response) => {
+  const user = users.find(
+    (user) =>
+      user.username === request.body.username &&
+      user.password === request.body.password
+  );
+  if (user) {
+    response.send(`${user.username} login successful`);
+  } else {
+    response.status(401).send('username or password is incorrect');
+  }
+});
 
 app.post('/api/users', (request, response) => {
   const newUser = request.body;
@@ -13,24 +47,27 @@ app.post('/api/users', (request, response) => {
 
   if (isNameknow) {
     response.status(409).send('User is available');
-  } else users.unshift(newUser.name);
+  } else users.unshift(newUser);
   response.send(`${newUser.name} added`);
 });
 
-app.delete('/api/users/:name', (request, response) => {
-  const usersIndex = users.indexOf(request.params.name);
-  if (usersIndex === -1) {
+app.delete('/api/users/:username', (request, response) => {
+  const user = users.find((user) => user.username === request.params.username);
+  if (user) {
+    const filtered = users.filter(
+      (user) => user.username != request.params.username
+    );
+    response.send(filtered);
+  } else {
     response.status(404).send('User ist not available');
-    return;
   }
-  users.splice(usersIndex, 1);
-  response.send('Deleted');
 });
 
-app.get('/api/users/:name', (request, response) => {
-  const isNameknow = users.includes(request.params.name);
-  if (isNameknow) {
-    response.send(request.params.name);
+app.get('/api/users/:username', (request, response) => {
+  const user = users.find((user) => user.username === request.params.username);
+
+  if (user) {
+    response.send(user);
   } else {
     response.status(404).send('User ist not available');
   }
