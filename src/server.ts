e6d27deb3a9e7648161700beb8006e46fd34c2be
ziedlassoued <zied.lassoued@outlook.de>
2021-10-28 11:37:36 +0200

    ignore .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { connectDatabase } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URL dotenv variable');
+}
 
 const app = express();
 const port = 3000;
@@ -100,6 +108,8 @@ app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
